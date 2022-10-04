2d5542c694688c44705cebb564f616f6e5776fc1
Bryan Johnson <btj117@msstate.edu>
2022-10-04 01:43:47 -0500

    Added edit profile button, edit profile page, and made CSS changes.

--- fakebook/settings.py
+++ fakebook/settings.py
@@ -112,7 +112,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = '/static/'
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
