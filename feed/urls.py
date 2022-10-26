d206cdcfbfd5ed0c8532e8d88830c4dfda9e9e26
Bryan Johnson <btj117@msstate.edu>
2022-10-26 11:45:52 -0500

    Profile, posts, comments, likes, and tags.

--- feed/urls.py
+++ feed/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import Index
+from .views import Index, CreatePost, PostListView
+from . import views
+
 
 urlpatterns = [
     path('', Index.as_view(), name='home'),
+    path('post/new/', CreatePost.as_view(), name='create_post'),
+    path('posts/', PostListView.as_view(), name='posts'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
+from django.views import View
+
 from .models import Profile, FriendRequest
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegistrationForm, UserUpdateForm, ProfileUpdateForm
+from feed.models import Post, Like
 from django.http import JsonResponse
 from django.http import HttpResponseRedirect
+from django.views.generic import ListView, UpdateView, DeleteView
 import random
 
 User = get_user_model()
@@ -130,38 +134,41 @@ def register(request):
     return render(request, 'users/register.html', {'form': form})
 
 
-def profile(request):
-    if request.user.is_authenticated:
-        active_profile = request.user.profile
-        active_user = active_profile.user
-        sent_friend_requests = FriendRequest.objects.filter(from_user=active_profile.user)
-        rec_friend_requests = FriendRequest.objects.filter(to_user=active_profile.user)
-        friends = active_profile.friends.all()
+class profile(View):
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            active_profile = request.user.profile
+            active_user = active_profile.user
+            sent_friend_requests = FriendRequest.objects.filter(from_user=active_profile.user)
+            rec_friend_requests = FriendRequest.objects.filter(to_user=active_profile.user)
+            friends = active_profile.friends.all()
+            posts = Post.objects.all().order_by('-date_posted')
 
-        button_status = 'none'
-        if active_profile not in request.user.profile.friends.all():
-            button_status = 'not_friend'
+            button_status = 'none'
+            if active_profile not in request.user.profile.friends.all():
+                button_status = 'not_friend'
 
-            # if we have sent him a friend request
-            if len(FriendRequest.objects.filter(
-                    from_user=request.user).filter(to_user=active_profile.user)) == 1:
-                button_status = 'friend_request_sent'
+                # if we have sent him a friend request
+                if len(FriendRequest.objects.filter(
+                        from_user=request.user).filter(to_user=active_profile.user)) == 1:
+                    button_status = 'friend_request_sent'
 
-            if len(FriendRequest.objects.filter(
-                    from_user=active_profile.user).filter(to_user=request.user)) == 1:
-                button_status = 'friend_request_received'
+                if len(FriendRequest.objects.filter(
+                        from_user=active_profile.user).filter(to_user=request.user)) == 1:
+                    button_status = 'friend_request_received'
 
-        context = {
-            'u': active_user,
-            'button_status': button_status,
-            'friends_list': friends,
-            'sent_friend_requests': sent_friend_requests,
-            'rec_friend_requests': rec_friend_requests,
-        }
-        return render(request, 'users/profile.html', context)
-    else:
-        messages.warning(request, f'Please log in to access your profile')
-        return redirect('login')
+            context = {
+                'u': active_user,
+                'button_status': button_status,
+                'friends_list': friends,
+                'sent_friend_requests': sent_friend_requests,
+                'rec_friend_requests': rec_friend_requests,
+                'posts': posts
+            }
+            return render(request, 'users/profile.html', context)
+        else:
+            messages.warning(request, f'Please log in to access your profile')
+            return redirect('login')
 
 
 def edit_profile(request):
