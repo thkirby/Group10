38fa3b7d47bfc28a84b2874dcc5f403f44c3b525
Bryan Johnson <btj117@msstate.edu>
2022-11-04 21:02:00 -0500

    Profile, posts, comments, likes, and sharing.

--- feed/admin.py
+++ feed/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Post, Comment, Like
 
+# adding admin functionality based on provided models
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(Like)
--- feed/forms.py
+++ feed/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import Comment, Post
 
 
+# form for creating a new comment and its fields
 class NewCommentForm(forms.ModelForm):
     comment = forms.CharField(label='', max_length=255, widget=forms.Textarea(
         attrs={'rows': '3', 'placeholder': 'Enter a comment...'}))
@@ -11,6 +12,7 @@ class NewCommentForm(forms.ModelForm):
         fields = ['comment']
 
 
+# form for creating a new post and its fields
 class NewPostForm(forms.ModelForm):
     description = forms.CharField(label='', widget=forms.Textarea(
         attrs={'rows': '4', 'placeholder': 'Type something...'}))
@@ -20,6 +22,7 @@ class NewPostForm(forms.ModelForm):
         fields = ['description', 'pic']
 
 
+# form for sharing a post and its fields
 class SharePostForm(forms.Form):
     description = forms.CharField(label='', widget=forms.Textarea(attrs={
                                 'rows': '4', 'placeholder': 'Type something...'}))
